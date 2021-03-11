9d7361fbc9726f02c3ab2efde4028812433d21a7
Alberto Martinez <j.alberto.martinez.d@gmail.com>
2021-03-11 12:49:44 -0600

    Agregar archivos de proyecto.

---
+++ CodeChallenge/Controllers/AnalyzeController.cs
@@ -0,0 +1,55 @@
+﻿using CodeChallenge.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CodeChallenge.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AnalyzeController : ControllerBase
+    {
+        [HttpPost]        
+        public IActionResult Get([FromBody] AnalyzeData data)
+        {
+            try
+            {
+                // Validate input
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+
+                // Remove white spaces
+                var onlyText = data.Text.Replace(" ", "");
+
+                // Count words
+                var wordCount = data.Text.Split(" ", StringSplitOptions.RemoveEmptyEntries).Length;
+                                
+                // Remove numbers
+                var onlyCharacters = Regex.Replace(onlyText, @"[\d-]", string.Empty);
+
+                // Count characters
+                var characterCount = onlyCharacters.OrderBy(c => c).GroupBy(c => c).ToDictionary(g => g.Key, g => g.Count());
+                
+                var result = new {
+                    textLength = new { withSpaces = data.Text.Length, withoutSpaces = onlyText.Length},
+                    wordCount,
+                    characterCount = JsonConvert.SerializeObject(characterCount)
+                };
+
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+            
+        }
+    }
+}
